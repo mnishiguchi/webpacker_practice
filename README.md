d87f38d75c95ce601bfe9bb8d9f0b36605994289
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-03-28 21:59:43 -0400

    Initial commit
    * Show Hello react component

---
+++ README.md
@@ -0,0 +1,6 @@
+# Webpacker practice
+
+## References
+
+- [http://weblog.rubyonrails.org/2017/2/23/Rails-5-1-beta1/](http://weblog.rubyonrails.org/2017/2/23/Rails-5-1-beta1/)
+- [https://www.driftingruby.com/episodes/ruby-on-rails-5-1-0-changes-and-new-features](https://www.driftingruby.com/episodes/ruby-on-rails-5-1-0-changes-and-new-features)
---
+++ app/javascript/packs/hello_react.jsx
@@ -0,0 +1,19 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+
+const Hello = props => (
+  <div>Hello {props.name}!</div>
+)
+
+Hello.defaultProps = {
+  name: 'David'
+}
+
+Hello.propTypes = {
+  name: React.PropTypes.string
+}
+
+export default Hello
